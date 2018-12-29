7a85b4bf18b92bb74f82a9cfc6e7fda96a5b3112
Satoshi Inaba <satoshi@SatoshinoMacBook-puro.local>
2018-12-29 16:36:29 +0700

    create function to check letter

--- src/hangman.js
+++ src/hangman.js
@@ -3,8 +3,26 @@ class Hangman {
         this.word = word.toLowerCase().split('')
         this.remainingGuesses = remainingGuesses
         this.status = 'playing'
-        this.message = ''
-        this.guessedLetters = ''
+        this.guessedLetters = []
+    }
+    get puzzle () {
+        let puzzle = ''
+        this.word.forEach((letter) => {
+            puzzle += this.guessedLetters.includes(letter) || letter === ' ' ? letter : '*'
+        })
+        return puzzle
+    }
+    checkLetter(letter) {
+        if (this.status !== 'playing') return
+
+        letter = letter.toLowerCase()
+        if (this.guessedLetters.includes(letter)) return
+
+        if (!this.word.includes(letter)) {
+            this.remainingGuesses--
+        }
+
+        this.guessedLetters.push(letter)
     }
 }
 
